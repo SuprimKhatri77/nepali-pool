0b9a6385a71a2c511c6642b937e06c4e605783c3
Suprim Khatri <Supremekhatri62@gmail.com>
2025-07-13 16:06:57 +0545

    integrated better auth,neon db, drizzle orm

---
+++ server/lib/auth/auth.ts
@@ -0,0 +1,39 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../../../lib/db";
+import { sendEmail } from "../send-email";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Reset Password",
+        html: `A reset password request was made from your side. <br> Click on the link to reset password ${url} <br> If it was not you, You can safely ignore this email.`,
+      });
+    },
+  },
+  emailVerification: {
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      await sendEmail({
+        to: user.email,
+        subject: "Verify your email",
+        html: `A request from your side was made for email verification. <br>Click on the link to verify your email ${url} <br> If it was not you , You can safely ignore this email.`,
+      });
+    },
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+    },
+  },
+  plugins: [nextCookies()],
+});
