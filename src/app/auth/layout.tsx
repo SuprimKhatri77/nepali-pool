8e46c879464cdfc2c3335efc1868f51e79bd1243
roshan393 <chanantony29@gmail.com>
2025-07-14 22:29:27 +0545

    signup form data sent succesfully with alignment of ui in pc

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,7 @@
 
 export default function AuthLayout({ children }: Readonly < { children: React.ReactNode }>) {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-to-r from-[#1F406B] to-[#D7FCFF]">
+    <main className="min-h-screen max-w-[1540px] mx-auto flex items-center justify-center bg-gradient-to-r from-[#1F406B] to-[#D7FCFF]">
         {children}
     </main>
   )
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,190 +1,166 @@
 "use client";
+
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState, useActionState } from "react";
+import { toast } from "sonner";
+import { FormState, SignUp } from "../../../../server/actions/signup";
+
 export default function SignUpPage() {
-  const [signupData, setSignupData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    role: "student",
-    agree: false,
-  });
-  const [formErrors, setFormErrors] = useState({
-    name: "",
-    email: "",
-    password: "",
-    role: "",
-    agree: "",
-  });
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value, type } = e.target;
-    const fieldValue =
-      type === "checkbox"
-        ? (e.target as HTMLInputElement).checked //  checkbox handling
-        : value;
-    setSignupData((prevData) => ({
-      ...prevData,
-      [name]: fieldValue,
-    }));
-    validateField(name, fieldValue);
+  const initialState: FormState = {
+    errors: {},
+    message: "",
+    success: false,
   };
+  const [state, formAction, isPending] = useActionState<FormState, FormData>(
+    SignUp,
+    initialState
+  );
 
-  const validateField = (name: string, value: string | boolean) => {
-    let error = "";
-    switch (name) {
-      case "name":
-        if (typeof value === "string" && value.length > 3 && !/^[A-Za-z\s]{2,40}$/.test(value)) {
-          error = "Only letters and spaces allowed (2–40 chars)";
-        }
-        break;
-      case "email":
-        if (
-          typeof value === "string" &&
-          !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)
-        ) {
-          error = "Invalid email format";
-        }
-        break;
-      case "password":
-        if (
-          typeof value === "string" &&
-          value.length < 6 &&
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/.test(value)
-        ) {
-          error = "Password must be at least 6 characters long and include uppercase, lowercase, number, and special character";
-        }
-        break;
-      case "agree":
-        if (!value) {
-          error = "You must agree to continue";
-        }
-        break;
-      default:
-        break;
+  const [role, setRole] = useState("student");
+
+  useEffect(() => {
+    if (state.message) {
+      toast.success(state.message);
     }
+  }, [state.message]);
 
-    setFormErrors((prevError) => ({
-      ...prevError,
-      [name]: error,
-    }));
-  };
-
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
-      e.preventDefault();
-   console.log("Form submitted with data:", signupData);
-  }
   return (
-    <div className="flex md:flex-row sm:flex-col flex-col-reverse gap-4 lg:gap-8 justify-between  items-start md:items-center p-8">
-      <div id="signup-image" className="max-w-96 md:max-w-[550px] lg:max-w-[818px] w-full">
-        <Image priority src="/signup.png" alt="signup" width={818} height={818} />
+    <div className="flex md:flex-row sm:flex-col flex-col-reverse gap-4 lg:gap-8 justify-between items-center p-8">
+      <div className="hidden md:hidden lg:max-w-[818px] lg:block w-full">
+        <Image
+          priority
+          src="/signup.png"
+          alt="signup"
+          width={700}
+          height={700}
+        />
       </div>
 
-      <div
-        id="signup-form"
-        className="max-w-[550px] w-full   flex items-end md:items-center justify-center"
-      >
-        <form onSubmit={handleSubmit} className="w-[90%] p-6 text-black bg-[#A6AEBF] rounded-[13px] shadow-md">
+      <div className="max-w-[550px] w-full flex items-center justify-center">
+        <form
+          action={formAction}
+          className="w-[90%] p-6 text-black bg-[#A6AEBF] rounded-[13px] shadow-md"
+        >
           <h2 className="text-2xl font-bold mb-3 text-center">
             Get Started Now
           </h2>
 
-          <label htmlFor="name" className="block  font-medium">
-            Name
-          </label>
-          <input
-            autoComplete="off"
-            type="text"
-            name="name"
-            value={signupData.name}
-            onChange={handleChange}
-            className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]"
-            placeholder="Enter your name"
-            required
-          />
-          {formErrors.name && (
-            <p className="text-red-600 text-sm mb-1">{formErrors.name}</p>
+          <div className="flex items-center justify-between p-1 gap-2">
+            <div id="firstname" className="flex flex-col gap-1">
+              <label htmlFor="firstname" className="block font-medium">
+                First Name
+              </label>
+              <input
+                autoComplete="off"
+                type="text"
+                name="firstname"
+                required
+                className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]
+  focus:border-blue-500 focus:ring-2  focus:ring-blue-300 
+             transition duration-200"
+                placeholder="first name"
+              />
+            </div>
+
+            <div id="lastname" className="flex flex-col gap-1">
+              <label htmlFor="lastname" className="block font-medium">
+                Last Name
+              </label>
+              <input
+                autoComplete="off"
+                type="text"
+                name="lastname"
+                required
+                className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]
+  focus:border-blue-500 focus:ring-2 focus:ring-blue-300 
+             transition duration-200"
+                placeholder="last name"
+              />
+            </div>
+          </div>
+          {state.errors?.firstname && (
+            <p className="text-red-600 text-sm mb-1">
+              {state.errors.firstname[0]}
+            </p>
+          )}
+          {state.errors?.lastname && (
+            <p className="text-red-600 text-sm mb-1">
+              {state.errors.lastname[0]}
+            </p>
           )}
 
-          <label htmlFor="email" className="block  font-medium">
+          <label htmlFor="email" className="block font-medium">
             Email
           </label>
           <input
             autoComplete="off"
-           
             type="email"
             name="email"
-            value={signupData.email}
-            onChange={handleChange}
-            className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]"
+            className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]
+            focus:border-blue-500 focus:ring-2 focus:ring-blue-300 
+             transition duration-200
+            "
             placeholder="Enter your email"
             required
           />
-          {formErrors.email && (
-            <p className="text-red-600 text-sm mb-1">{formErrors.email}</p>
+          {state.errors?.email && (
+            <p className="text-red-600 text-sm mb-1">{state.errors.email[0]}</p>
           )}
 
-          <label htmlFor="password" className="block  font-medium">
+          <label htmlFor="password" className="block font-medium">
             Password
           </label>
           <input
+            autoComplete="off"
             type="password"
             name="password"
-            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$"
-            value={signupData.password}
-            onChange={handleChange}
-            className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]"
+            
+            className="max-w-[440px] w-full p-2 outline-none mb-2 rounded border border-[#333446]
+            focus:border-blue-500 focus:ring-2 focus:ring-blue-300 
+             transition duration-200"
             placeholder="Enter your password"
             required
           />
-          {formErrors.password && (
-            <p className="text-red-600 text-sm mb-1">{formErrors.password}</p>
+          {state.errors?.password && (
+            <p className="text-red-600 text-sm mb-1">
+              {state.errors.password[0]}
+            </p>
           )}
 
-          <label htmlFor="role" className="block  font-medium">
+          <label htmlFor="role" className="block font-medium">
             Role
           </label>
           <select
             name="role"
-            
-            value={signupData.role}
-            onChange={handleChange}
-            className="max-w-[440px] w-full bg-[#A6AEBF] p-2 outline-none mb-3 rounded border border-[#333446]"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            className="max-w-[440px] w-full bg-[#A6AEBF] p-2 outline-none mb-3 rounded border border-[#333446]
+            focus:border-blue-500 focus:ring-2 focus:ring-blue-300 
+             transition duration-200"
           >
             <option value="student">Student</option>
             <option value="mentor">Mentor</option>
           </select>
+          <input type="hidden" name="role" value={role} />
 
-          <div className="flex items-center mb-1">
-            <input
-              type="checkbox"
-              name="agree"
-              checked={signupData.agree}
-              onChange={handleChange}
-              className="mr-2"
-            />
-            <span>I agree to terms and policy</span>
-          </div>
-          {formErrors.agree && (
-            <p className="text-red-600 text-sm mb-1">{formErrors.agree}</p>
+          {state.errors?.role && (
+            <p className="text-red-600 text-sm mb-1">{state.errors.role[0]}</p>
           )}
 
           <button
             type="submit"
-            
+            disabled={isPending}
             className="max-w-[440px] cursor-pointer w-full p-2 outline-none 
               bg-gradient-to-r from-[#4ED7F1] to-[#5C6CF2] 
               hover:from-[#5C6CF2] hover:to-[#4ED7F1] 
               transition-all duration-300 ease-in-out rounded font-bold"
           >
-            Sign Up
+            {isPending ? "Submitting..." : "Sign Up"}
           </button>
 
           <p className="mt-3 text-center text-sm">
-            Have an account?&nbsp;<a
+            Have an account?&nbsp;
+            <a
               href="/auth/login"
               className="text-[#0F3DDE] hover:underline font-medium"
             >
