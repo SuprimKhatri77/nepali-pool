bff09b1e3e5e1da7aaaef63319016a79e6f086c7
Suprim Khatri <Supremekhatri62@gmail.com>
2025-07-23 21:29:51 +0545

    added mentor application status changes by admin functionality

---
+++ server/actions/mentorApplication.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { db } from "../../lib/db";
+import { mentorProfile, user } from "../../lib/db/schema";
+import { sendEmail } from "../lib/send-email";
+
+export type FormState = {
+  errors?: {
+    applicationId?: string;
+  };
+  message?: string;
+  success?: boolean;
+  redirectTo?: string;
+};
+
+export async function AcceptMentorApplication(
+  prevState: FormState,
+  formData: FormData
+) {
+  const applicationId = formData.get("applicationId") as string;
+  if (!applicationId) {
+    return {
+      errors: {
+        applicationId: "Application ID is required",
+      },
+      message: "Application ID not found!",
+      success: false,
+    };
+  }
+
+  try {
+    const [userRecord] = await db
+      .select()
+      .from(user)
+      .where(eq(user.id, applicationId));
+    if (!userRecord) {
+      return {
+        errors: {},
+        message: "User Record not found!",
+        success: false,
+        redirectTo: "/sign-up",
+      };
+    }
+    await db
+      .update(mentorProfile)
+      .set({
+        verifiedStatus: "accepted",
+        updatedAt: new Date(),
+      })
+      .where(eq(mentorProfile.userId, applicationId));
+    await sendEmail({
+      to: userRecord.email,
+      subject: "Mentor Application Status",
+      text: "Congratulations!, Your Mentor Application has been accepted.<br>",
+      html: `You can now Access the Mentor Dashboard and Its features.`,
+    });
+    return {
+      erros: {},
+      message: "Application Accepted!",
+      success: true,
+      redirectTo: `/mentor-applications/${applicationId}`,
+    };
+  } catch (error) {
+    console.error("Error: ", error);
+    return {
+      errors: {
+        applicationId: "Invalid error!",
+      },
+      message: "Something went wrong!",
+      success: true,
+    };
+  }
+}
--- src/components/AdminPage.tsx
+++ src/components/AdminPage.tsx
@@ -1,10 +1,9 @@
-import { ne } from "drizzle-orm"
 import { db } from "../../lib/db"
-import { mentorProfile, MentorProfileSelectType } from "../../lib/db/schema"
+import { mentorProfile, MentorProfileSelectType, user } from "../../lib/db/schema"
 import { redirect } from "next/navigation"
 import { auth } from "../../server/lib/auth/auth"
 import { headers } from "next/headers"
-import { Button } from "./ui/button"
+import Link from "next/link"
 
 export default async function AdminPage() {
     const session = await auth.api.getSession({
@@ -33,9 +32,8 @@ export default async function AdminPage() {
                         <div key={mentor.userId} className="bg-gray-200 py-5 px-7 rounded-lg flex flex-col gap-2 items-center">
                             <p>{mentor.user.name}</p>
                             <div className="flex gap-2">
+                                <Link href={`/mentor-applications/${mentor.userId}`} className="bg-blue-400 py-2 px-4 rounded-lg hover:bg-blue-500 transition-all duration-300 ease-in">View Details</Link>
 
-                                <Button>Accept</Button>
-                                <Button variant="destructive">Reject</Button>
                             </div>
                         </div>
                     ))
