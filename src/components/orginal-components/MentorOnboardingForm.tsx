c0d9ce0787e9e532d4d7b28669d5e088d5133b34
Bsal <bishalm626@gmail.com>
2025-07-24 21:53:06 +0545

    onboarding polish

--- src/components/orginal-components/MentorOnboardingForm.tsx
+++ src/components/orginal-components/MentorOnboardingForm.tsx
@@ -31,15 +31,15 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
     }
   },[state.message])
   return (
-    <div id="form-container" className="rounded-sm text-[#0f172a] bg-gray-200 max-w-[600px] lg:max-w-[1000px] w-full p-4 m-4">
+    <div id="form-container" className="shadow-2xl rounded-sm text-[#0f172a] bg-gray-200 max-w-[600px] lg:max-w-[1000px] w-full p-4 m-4">
       <Logo />
-      <h1 className="text-3xl font-medium my-2 text-center lg:text-left md:pl-0 lg:pl-12 ">Onboarding</h1>
+      <h1 className="text-3xl text-shadow-md font-medium my-2 text-center lg:text-left md:pl-0 lg:pl-12 ">Onboarding</h1>
       <form action={formAction} className="flex flex-col gap-8 justify-center">
         <input type="text"  defaultValue={currentUserId} className="hidden" name="userId"/>
         <div id="inputs-container" className="flex lg:flex-row flex-col-reverse gap-8 justify-center">
           <div id="left-column" className="flex flex-col gap-4">
             <div>
-              <label htmlFor="country" className="font-medium text-base">
+              <label htmlFor="country" className="font-medium text-shadow-sm text-base">
                 Current Country:
               </label>
               <input
@@ -47,13 +47,13 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                 name="country"
                 id="country"
                 placeholder="eg: Japan"
-                className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200 w-full"
               />
             </div>
             <div>
-              <label htmlFor="nationality" className="font-medium text-base">
+              <label htmlFor="nationality" className="text-shadow-sm font-medium text-base">
                 Nationality:
               </label>
               <input
@@ -61,14 +61,14 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                 name="nationality"
                 id="nationality"
                 placeholder="eg: Nepali"
-                className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200 w-full"
               />
             </div>
             <div id="city-zip-container" className="flex md:flex-row flex-col gap-4 w-full">
               <div id="city-container" className="flex flex-col">
-                <label htmlFor="city" className="font-medium text-base">
+                <label htmlFor="city" className="text-shadow-sm font-medium text-base">
                   City:
                 </label>
                 <input
@@ -76,13 +76,13 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                   name="city"
                   id="city"
                   placeholder="eg: Kathmandu"
-                  className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                  className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200"
                 />
               </div>
               <div id="zip-container" className="flex flex-col">
-                <label htmlFor="zip" className="font-medium text-base">
+                <label htmlFor="zip" className="text-shadow-sm font-medium text-base">
                   Zip Code:
                 </label>
                 <input
@@ -90,14 +90,14 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                   name="zip"
                   id="zip"
                   placeholder="eg: 44600"
-                  className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                  className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200"
                 />
               </div>
             </div>
             <div>
-              <label htmlFor="phonenumber" className="font-medium text-base">
+              <label htmlFor="phonenumber" className="text-shadow-sm font-medium text-base">
                 Phone Number
               </label>
               <input
@@ -105,45 +105,45 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                 name="phonenumber"
                 id="phonenumber"
                 placeholder="eg: 9800000000"
-                className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200 w-full"
               />
             </div>
             <div id="gender">
-              <label htmlFor="gender" className="font-medium text-base">
+              <label htmlFor="gender" className="text-shadow-sm font-medium text-base">
                 Gender:
               </label>
               <div className="flex gap-2 items-center justify-center">
                 <div className="flex items-center gap-2">
                   <input type="radio" name="gender" id="male" value="male" />
-                  <label htmlFor="male">Male</label>
+                  <label htmlFor="male" className="text-shadow-2xs">Male</label>
                 </div>
                 <div className="flex items-center gap-2">
                   <input type="radio" name="gender" id="female" value="female" />
-                  <label htmlFor="female">Female</label>
+                  <label htmlFor="female" className="text-shadow-2xs">Female</label>
                 </div>
                 <div className="flex items-center gap-2">
                   <input type="radio" name="gender" id="other" value="other" />
-                  <label htmlFor="other">Other</label>
+                  <label htmlFor="other" className="text-shadow-2xs">Other</label>
                 </div>
               </div>
             </div>
             <div>
-              <label htmlFor="resume" className="font-medium text-base">
+              <label htmlFor="resume" className="text-shadow-sm font-medium text-base">
                 Resume:
               </label>
               <input
                 type="file"
                 name="resume"
                 id="resume"
-                className="border border-blue-500  p-2 w-full"
+                className="shadow-xl border border-blue-500  p-2 w-full"
               />
             </div>
           </div>
           <div
             id="right-column"
-            className="rounded-sm bg-gradient-135 p-4 border-[3px] border-[#ACACAC]"
+            className="shadow-2xl rounded-sm bg-gradient-135 p-4 border-[3px] border-[#ACACAC]"
           >
             <div id="image-container" className="flex justify-center">
               <button
@@ -153,7 +153,7 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                 onClick={handleProfileImageClick}
               >
                 {/* Circular image container */}
-                <div className="w-28 h-28 rounded-full overflow-hidden">
+                <div className="w-28 h-28 rounded-full shadow-xl overflow-hidden">
                   <Image
                     src={preview}
                     alt="profile"
@@ -180,20 +180,20 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
 
             <div id="input-section-right">
               <div>
-                <label htmlFor="citizensip" className="font-medium text-base">
-                  Citizensip:
+                <label htmlFor="citizensip" className="text-shadow-sm font-medium text-base">
+                  Citizenship:
                 </label>
                 <input
                   type="file"
                   name="citizensip"
                   id="citizensip"
-                  className=" w-full  p-2 outline-none mb-3  rounded border border-[#333446]
+                  className="shadow-xl w-full  p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200"
                 />
               </div>
               <div className="flex flex-col">
-                <label htmlFor="bio" className="font-medium text-base">
+                <label htmlFor="bio" className="text-shadow-sm font-medium text-base">
                   Bio:
                 </label>
                 <textarea
@@ -201,7 +201,7 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
                   maxLength={200}
                   minLength={50}
                   placeholder="Write about yourself"
-                  className=" p-2 outline-none mb-3  rounded border border-[#333446]
+                  className="shadow-xl p-2 outline-none mb-3  rounded border border-[#333446]
             focus:border-blue-500 focus:ring-2 focus:ring-blue-500 placeholder:text-[#1F406B]
              transition duration-200 w-full max-h-[200px] min-h-[100px] h-full"
                 ></textarea>
@@ -211,10 +211,10 @@ export default function MentorOnboardingForm({currentUserId}:{readonly currentUs
         </div>
 
         <div className="flex justify-center flex-col items-center mt-8">
-          <button disabled={isPending} type="submit" className="bg-[#4ed7f1] hover:bg-[#46a9bd] transition duration-200 py-2 px-4 rounded w-1/2 font-medium text-base cursor-pointer">
+          <button disabled={isPending} type="submit" className="shadow-xl bg-[#4ed7f1] hover:bg-[#46a9bd] transition duration-200 py-2 px-4 rounded w-1/2 font-medium text-base cursor-pointer">
           {isPending ? "Submitting..." : "Submit"}
           </button>
-          <p className="mt-4">Already have an account?&nbsp;<Link href="/login" className="font-medium hover:underline">Login</Link></p>
+          <p className="text-shadow-md mt-4">Already have an account?&nbsp;<Link href="/login" className="font-medium hover:underline">Login</Link></p>
         </div>
       </form>
     </div>
