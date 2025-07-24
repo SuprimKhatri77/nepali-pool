11be25a3be30f71378dc2d4823b0b2a4e5c422f0
roshan393 <chanantony29@gmail.com>
2025-07-24 08:50:25 +0545

    bun lock edited

---
+++ src/app/(auth)/sign-up/onboarding/mentor/page.tsx
@@ -0,0 +1,5 @@
+import MentorOnboardingForm from "@/components/OnboardingMentorForm"    
+
+export  default function onboardingMentorPage(){
+    return <MentorOnboardingForm  currentUserId="123"/> 
+}
