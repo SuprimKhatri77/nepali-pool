fbc50de28c87ff183356a444a679ba791431cc1c
Bsal <bishalm626@gmail.com>
2025-08-28 17:35:37 +0545

    icons added

--- src/components/orginal-components/PUBLIC/reusableComponents/TRUSTEDBY.tsx
+++ src/components/orginal-components/PUBLIC/reusableComponents/TRUSTEDBY.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
+
 export default function TrustedByCard({
   Header,
   Count,
+  img,
 }: {
   readonly Header: string;
   readonly Count: number;
+  readonly img: string;
 }) {
   return (
     <div className="max-w-[320px] min-h-[350px] shadow-md w-full rounded-2xl bg-gradient-to-b from-[#E9F3FF]  to-[#AAAEC8] from-0%  to-100% flex items-center justify-center flex-col">
       <div className="mb-3 ">
-        <p>Icon Here?</p>
-        <p className="text-xl font-medium">{Header}</p>
+        <Image src={img} alt="imagebytrust" height={128} width={128} />
+        <p className="text-xl text-center font-medium">{Header}</p>
       </div>
       <div>
         <h1 className="text-5xl font-bold">{Count}+</h1>
--- src/components/orginal-components/PUBLIC/trustedBy/page.tsx
+++ src/components/orginal-components/PUBLIC/trustedBy/page.tsx
@@ -9,10 +9,10 @@ export default function TrustedBy() {
       <hr className="w-30 h-1 bg-gradient-to-r from-[#ffffff] via-[#D83A0A] to-[#ffffff] mx-auto" />
       <div id="container" className="mt-3 flex gap-6 flex-wrap justify-center">
         {/* load cards here! */}
-        <TrustedByCard Header="School" Count={20} />
-        <TrustedByCard Header="Partner With" Count={20} />
-        <TrustedByCard Header="Students" Count={20} />
-        <TrustedByCard Header="Mentor" Count={20} />
+        <TrustedByCard img="/school.svg" Header="School" Count={20} />
+        <TrustedByCard img="/partner.svg" Header="Partner With" Count={20} />
+        <TrustedByCard img="/student.svg"Header="Students" Count={20} />
+        <TrustedByCard img="/mentor.svg" Header="Mentor" Count={20} />
       </div>
     </section>
   );
