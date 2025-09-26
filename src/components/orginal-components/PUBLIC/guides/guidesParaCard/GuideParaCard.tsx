e80389087f1d2119cbc4f7b986aba0722a6b30e3
roshan393 <chanantony29@gmail.com>
2025-09-26 19:19:29 +0545

    visa ul li rendered with code improved studentvisaapplicationprocess.tsx

--- src/components/orginal-components/PUBLIC/guides/guidesParaCard/GuideParaCard.tsx
+++ src/components/orginal-components/PUBLIC/guides/guidesParaCard/GuideParaCard.tsx
@@ -1,13 +1,24 @@
-
-
 type GuideParaCardProps = {
   step: number | string;
   title: string;
   description: string;
   isLast?: boolean;
+  ul?: string[];
 };
 
-export default function GuideParaCard({ step, title, description, isLast }: GuideParaCardProps) {
+export default function GuideParaCard({
+  step,
+  title,
+  description,
+  isLast = false,
+  ul = [],
+}: GuideParaCardProps) {
+  const li: string[] = [];
+  if (ul.length > 0) {
+    for (const list of ul) {
+      li.push(list);
+    }
+  }
   return (
     <div className="flex flex-col items-start relative px-8 pt-5">
       <div className="flex flex-col items-center absolute left-0 h-full">
@@ -19,11 +30,22 @@ export default function GuideParaCard({ step, title, description, isLast }: Guid
             className="stroke-black stroke-1 fill-[#3A86FF]"
           />
         </svg>
-        {!isLast && <div className="absolute w-[2px] h-full bg-black flex-1 mt-0 top-[15]" />}
+        {!isLast && (
+          <div className="absolute w-[2px] h-full bg-black flex-1 mt-0 top-[15]" />
+        )}
       </div>
       <span className="text-base">step {step}</span>
       <h1 className="text-[#3A86FF] text-lg font-semibold">{title}</h1>
       <p className="text-sm">{description}</p>
+      {li.length > 0 && (
+        <ul className="pl-5">
+          {li.map((list, key) => (
+            <li className="list-disc" key={key}>
+              {list}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
+  );
 }
