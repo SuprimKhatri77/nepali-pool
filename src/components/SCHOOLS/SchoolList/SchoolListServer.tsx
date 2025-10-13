f935101e4eeb82a348b21239cf4bb2230c1f9595
roshan393 <chanantony29@gmail.com>
2025-10-13 08:51:35 +0545

    Removed unwanted server file in  components of sch ools page

--- src/components/SCHOOLS/SchoolList/SchoolListServer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { db } from "../../../../lib/db";
-import { SchoolSelectType } from "../../../../lib/db/schema";
-import SchoolList from './page';
-export default async function SchoolListServer() {
-  const schools: SchoolSelectType[] = await db.query.school.findMany();
-
-  return <SchoolList schools={schools}/>
-}
